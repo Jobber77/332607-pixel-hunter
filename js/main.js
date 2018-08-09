7ded24aecc3fd428998e62f523804dd43cc35dfa
Jobber77 <joalexey@gmail.com>
2018-08-09 10:07:13 +0300

    fixes after mentor feedback

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
-(function () {
+{
   // #region  variables
-  const SCREENS_ORDER_BY_ID = {
+  const ScreenOrderById = {
     0: `intro`,
     1: `greeting`,
     2: `rules`,
@@ -12,18 +12,18 @@
     7: `modal-error`,
     8: `modal-confirm`
   };
-  const KEYBOARD_KEYKODES = {
-    left: 37,
-    right: 39
+  const KeyboardKeykodes = {
+    LEFT: 37,
+    RIGHT: 39
   };
-  const STYLES = {
-    styleRuleForWrap: `.arrows__wrap {
+  const Styles = {
+    RULE_FOR_WRAP: `.arrows__wrap {
       position: absolute;
       top: 95px;
       left: 50%;
       margin-left: -56px;
     }`,
-    styleRuleForButtons: `.arrows__btn {
+    RULE_FOR_BUTTONS: `.arrows__btn {
       background: none;
       border: 2px solid black;
       padding: 5px 20px;
@@ -33,24 +33,37 @@
   const mainScreen = document.querySelector(`#main`);
   let currentScreenId = 0;
   // #endregion
-  (function initializeGame() {
+  const initializeGame = () => {
     orderScreens();
     showScreen(currentScreenId);
     drawArrowButtons();
     setArrowListeners();
     document.addEventListener(`keydown`, onArrowKeyPress);
-  })();
+  };
 
-  function orderScreens() {
-    for (const key of Object.keys(SCREENS_ORDER_BY_ID)) {
-      screens.find((item) => item.id === SCREENS_ORDER_BY_ID[key]).order = key;
+  const createDOMElement = (type, classes, textContent) => {
+    const newElement = document.createElement(type);
+    if (Array.isArray(classes)) {
+      classes.forEach((element) => newElement.classList.add(element));
+    } else {
+      newElement.classList.add(classes);
+    }
+    if (textContent) {
+      newElement.textContent = textContent;
+    }
+    return newElement;
+  };
+
+  const orderScreens = () => {
+    for (const key of Object.keys(ScreenOrderById)) {
+      screens.find((item) => item.id === ScreenOrderById[key]).order = key;
     }
     screens.sort((item1, item2) => item1.order > item2.order ? 1 : -1);
     // temp fix to hide service screens
     screens.pop();
     screens.pop();
-  }
-  function showScreen(screenOrder) {
+  };
+  const showScreen = (screenOrder) => {
     const fragment = document.createDocumentFragment();
     const screenTemplate = screens[screenOrder].content;
     const clonnedScreen = screenTemplate.cloneNode(true);
@@ -58,41 +71,42 @@
     mainScreen.innerHTML = ``;
     mainScreen.appendChild(fragment);
   }
-  function nextScreen() {
+  const nextScreen = () => {
     currentScreenId = currentScreenId < (screens.length - 1) ? ++currentScreenId : 0;
     showScreen(currentScreenId);
-  }
-  function previousScreen() {
+  };
+  const previousScreen = () => {
     currentScreenId = currentScreenId > 0 ? --currentScreenId : screens.length - 1;
     showScreen(currentScreenId);
-  }
-  function drawArrowButtons() {
-    const wrap = window.util.createDOMElement(`div`, `arrows__wrap`);
-    const leftArrow = window.util.createDOMElement(`button`, `arrows__btn`, `<-`);
-    const rightArrow = window.util.createDOMElement(`button`, `arrows__btn`, `->`);
+  };
+  const drawArrowButtons = () => {
+    const wrap = createDOMElement(`div`, `arrows__wrap`);
+    const leftArrow = createDOMElement(`button`, `arrows__btn`, `<-`);
+    const rightArrow = createDOMElement(`button`, `arrows__btn`, `->`);
     const style = document.createElement(`style`);
-    style.innerHTML = STYLES.styleRuleForWrap + STYLES.styleRuleForButtons;
+    style.innerHTML = Styles.RULE_FOR_WRAP + Styles.RULE_FOR_BUTTONS;
     wrap.appendChild(style);
     wrap.appendChild(leftArrow);
     wrap.appendChild(rightArrow);
     document.querySelector(`body`).appendChild(wrap);
-  }
-  function setArrowListeners() {
+  };
+  const setArrowListeners = () => {
     const buttons = Array.from(document.querySelectorAll(`.arrows__btn`));
     buttons.forEach((button) => button.addEventListener(`click`, onArrowButtonClick));
-  }
-  function onArrowKeyPress(evt) {
-    if (evt.keyCode === KEYBOARD_KEYKODES.left) {
+  };
+  const onArrowKeyPress = (evt) => {
+    if (evt.keyCode === KeyboardKeykodes.LEFT) {
       previousScreen();
-    } else if (evt.keyCode === KEYBOARD_KEYKODES.right) {
+    } else if (evt.keyCode === KeyboardKeykodes.RIGHT) {
       nextScreen();
     }
-  }
-  function onArrowButtonClick(evt) {
+  };
+  const onArrowButtonClick = (evt) => {
     if (evt.target.innerText === `<-`) {
       previousScreen();
     } else if (evt.target.innerText === `->`) {
       nextScreen();
     }
-  }
-})();
+  };
+  initializeGame();
+}
